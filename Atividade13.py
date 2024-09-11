960581037b30aa1f1c497504d8b7acbdeee49e1f
LainWeire <lucasfrota3000@gmail.com>
2024-09-11 15:23:58 -0300

    uff numeros

---
+++ Atividade13.py
@@ -0,0 +1,10 @@
+# Crie um programa que receba um número do usuário e informe se ele é positivo, negativo ou zero.
+n = float(input("Digite um número: "))
+if (n < 0):
+    print ("É negativo.")
+
+if (n == 0):
+    print ("É zero")
+
+if (n > 0):
+    print ("É positivo")
